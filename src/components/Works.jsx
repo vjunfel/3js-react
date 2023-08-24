70da0f49a071b540f78a7db08171c71dae7a0ce1
vjunfel <vjunfel@gmail.com>
2023-08-24 22:59:30 +0800

    added header logo icon and Work content section.

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,13 +1,88 @@
 import React from 'react'
 import { styled } from 'styled-components'
 
+const data = [
+  "Web Design",
+  "Development",
+  "Illustration",
+  "Product Design",
+  "Social Media",
+]
+
 const Section = styled.div`
   height: 100vh;
-  background-color: #ca58ff;
+  scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
 `
+
+const Container = styled.div`
+  width: 1000px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Left = styled.div`
+  flex:1;
+  display: flex;
+  align-items: center;
+`
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+
+const ListItem = styled.li`
+  font-size: 80px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+
+  ::after{
+    content: "${(props)=>props.text}";
+    position: absolute;
+    top: 0;
+    left: 0;
+    white-space: nowrap;
+  }
+
+  &:hover{
+    ::after{
+      animation: moveText 0.5s linear both;
+
+      @keyframes moveText {
+        to{
+          width: 100%;
+        }
+      }
+    }
+  }
+`;
+
+const Right = styled.div`
+  flex:1;
+`
+
 const Works = () => {
   return (
-    <div>Works</div>
+    <Section> 
+      <Container>
+        <Left>
+          <List>
+            {data.map((item)=> (
+            <ListItem key={item} text={item}> {item} </ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right>
+
+        </Right>
+      </Container>
+    </Section>
   )
 }
 
